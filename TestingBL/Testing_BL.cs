e59957a6bd29ba240dc5d7ba0a9f3ebbdbd96930
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2021-11-11 15:50:09 +0630

    pnz

---
+++ TestingBL/Testing_BL.cs
@@ -0,0 +1,12 @@
+﻿using CKM_CommonFunction;
+using CKM_DataLayer;
+using System.Data.SqlClient;
+
+
+namespace TestingBL
+{
+    public class Testing_BL
+    {
+
+    }
+}
--- Testing_Model/TestingModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Testing_Model
-{
-    public class TestingModel
-    {
-    }
-}
