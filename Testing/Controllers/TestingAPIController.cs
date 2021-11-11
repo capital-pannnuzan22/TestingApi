8ce1af0b3954401970a46374d5fb8a2bdc39183d
Pann Nu Zan <Pann Nu Zan@DESKTOP-52825H4>
2021-11-11 14:19:14 +0630

    pnz

--- Testing/Controllers/TestingAPIController.cs
+++ Testing/Controllers/TestingAPIController.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
+using TestingBL;
+using TestingModel;
 
 namespace Testing.Controllers
 {
     public class TestingAPIController : ApiController
     {
+       
+        //[HttpPost]
+        //[ActionName("GetLanguage")]
+        //public IHttpActionResult GetLanguage([FromBody] TestingModel languageModel)
+        //{
+        //    LanguageBL languageBL = new LanguageBL();
+        //    return Ok(languageBL.GetLanguage(languageModel));
+        //}
     }
 }
