187261f7c0908d7cd151c86737f860d93378b6d1
Htet Htet Win <capital.htethtetwin@gmail.com>
2021-11-11 16:39:34 -0800

    hhw

---
+++ Animal_Model/BaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Model
+{
+    class BaseModel
+    {
+        public string Mode { get; set; }
+        public string SPName { get; set; }
+        public SqlParameter[] Sqlprms { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedDate { get; set; }
+        public string param1 { get; set; }
+        public string param2 { get; set; }
+    }
+}
